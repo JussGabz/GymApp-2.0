b92a3461f7e646e4efeeaa9e743ba727e5459889
JussGabz <gabrieli94@hotmail.co.uk>
2024-01-07 18:23:30 +0000

    create routers for exercise and workout plan on api

---
+++ gym_app/admin_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'exercises', views.ExerciseViewSet, basename='exercise')
+router.register(r'workoutplans', views.WorkoutPlanViewSet, basename='workoutplan')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
