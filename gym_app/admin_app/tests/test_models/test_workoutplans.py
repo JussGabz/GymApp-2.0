276ce255ede465cfa564dd64ace1570afaeb42ae
JussGabz <gabrieli94@hotmail.co.uk>
2024-01-18 11:38:23 +0000

    test removing exercise from workout plan

--- gym_app/admin_app/tests/test_models/test_workoutplans.py
+++ gym_app/admin_app/tests/test_models/test_workoutplans.py
@@ -21,3 +21,25 @@ def test_workplan_add_exercise(test_exercise, test_workout_plan):
     # Query workout plan exercise by name
     added_exercise = test_workout_plan.exercise_set.filter(name="test_exercise").first()
     assert added_exercise == test_exercise
+
+@pytest.mark.django_db
+def test_work_remove_exercise(test_workout_plan, test_exercise):
+    """
+    Check exercise is removed from workout plan
+    """
+
+    # Add Exercise to workout plan
+    test_workout_plan.add_exercise(test_exercise)
+
+    # Verify Exercise is added to Workout Plan
+    exercise = test_workout_plan.exercise_set.filter(name="test_exercise").first()
+    assert exercise.name == "test_exercise"
+
+    # Remove Exercise from Workput Plan
+    test_workout_plan.remove_exercise(exercise)
+
+    # Verify that exercise has been removed from workout plan
+    assert test_workout_plan.exercise_set.filter(name="test_exercise").first() == None
+
+
+
