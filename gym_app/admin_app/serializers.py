0654ea63f9acbfd4b93164d49fc5a3d9616267d5
JussGabz <gabrieli94@hotmail.co.uk>
2024-01-21 13:15:58 +0000

    serializer for date

--- gym_app/admin_app/serializers.py
+++ gym_app/admin_app/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import Group, User
 from .models import Exercise, WorkoutPlan
 from rest_framework import serializers
+from gym_app.settings import DATETIME_FORMAT
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -8,6 +9,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'username', 'email', 'groups']
 
 class ExerciseSerializer(serializers.ModelSerializer):
+    date_added = serializers.DateTimeField(read_only=True, format=DATETIME_FORMAT)
+    
     class Meta:
         model = Exercise
         fields = ['id', 'name', 'target_area', 'date_added']
