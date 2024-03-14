f64caf1c34f366c4ceaed5d08e4941c1f4cef1bf
JussGabz <gabrieli94@hotmail.co.uk>
2024-03-14 20:55:00 +0000

    test exercise serializer

---
+++ gym_app/admin_app/tests/test_serializers/test_exercise_serializers.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from gym_app.admin_app.models import Exercise, WorkoutPlan
+from gym_app.admin_app.serializers import ExerciseSerializer, WorkoutPlanSerializer
+
+class TestExerciseSerializer(TestCase):
+    def setUp(self):
+        # Create sample data for test
+        self.valid_data = {"name": "Test Exercise", "target_area":"Test Area"}
+        self.invalid_data = {"name": "Test Exercise"}
+
+    def test_valid_serializer_data(self):
+        serializer = ExerciseSerializer(data=self.valid_data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.errors, {}) # No Errors Expected
+    
+    def test_invalid_serializer_data(self):
+        serializer = ExerciseSerializer(data=self.invalid_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertTrue('target_area' in serializer.errors)
+    
+    def test_valid_serializer_save(self):
+        serializer = ExerciseSerializer(data=self.valid_data)
+        self.assertTrue(serializer.is_valid())
+        instance = serializer.save()
+        self.assertIsInstance(instance, Exercise)
