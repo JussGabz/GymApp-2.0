1d4c123e1037b6f7ac075dbe637e5cc22cd42aaa
JussGabz <gabrieli94@hotmail.co.uk>
2024-03-14 20:55:28 +0000

    remove redundant functions

--- gym_app/admin_app/models.py
+++ gym_app/admin_app/models.py
@@ -11,31 +11,6 @@ class WorkoutPlan(models.Model):
     workout_type = models.CharField(max_length=50)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    def add_exercise(self, exercise):
-        try:
-            exercise.workoutplans.add(self)
-        except Exception as e:
-            raise e
-        return exercise
-
-    def remove_exercise(self, exercise):
-
-        # Check if exercise is apart of workout plan 
-        exercise_exists = self.exercise_set.filter(name=exercise.name).first()
-        # If apart of workout plan 
-
-        if not exercise_exists:
-            print("Exercise not in workout plan")
-            return
-
-        # Remove Exercise from Workoutplan
-        try:
-            self.exercise_set.remove(exercise)
-            print("Exercise removed from Workout Plan")
-        except Exception as e:
-            raise e
-    
-        
 
 class Exercise(models.Model):
     # TODO - Add Date Updated Field for exercise
@@ -43,5 +18,7 @@ class Exercise(models.Model):
     target_area = models.CharField(max_length=50)
     workoutplans = models.ManyToManyField(WorkoutPlan)
     date_added = models.DateTimeField(auto_now_add=True)
+
+
 
     
