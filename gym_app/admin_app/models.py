18b1b85417885b0b104f4dfb55cd9f26d3d2b374
JussGabz <gabrieli94@hotmail.co.uk>
2024-03-17 20:38:14 +0000

    switch many to many field for comphrension

--- gym_app/admin_app/models.py
+++ gym_app/admin_app/models.py
@@ -4,20 +4,20 @@ from django.db import models
 # Create your models here.
 
 
+class Exercise(models.Model):
+    # TODO - Add Date Updated Field for exercise
+    name = models.CharField(max_length=50, null=False)
+    target_area = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
 class WorkoutPlan(models.Model):
 
     # TODO - Add Date Updated Field for exercise
     name = models.CharField(max_length=50)
     workout_type = models.CharField(max_length=50)
     date_added = models.DateTimeField(auto_now_add=True)
-
-
-class Exercise(models.Model):
-    # TODO - Add Date Updated Field for exercise
-    name = models.CharField(max_length=50, null=False)
-    target_area = models.CharField(max_length=50)
-    workoutplans = models.ManyToManyField(WorkoutPlan)
-    date_added = models.DateTimeField(auto_now_add=True)
+    exercises = models.ManyToManyField(Exercise)
 
 
 
