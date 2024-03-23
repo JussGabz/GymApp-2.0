a62767306b1c7c44137285ec1b8e28074d88a526
JussGabz <gabrieli94@hotmail.co.uk>
2024-03-23 19:17:57 +0000

    Build Menu Component

---
+++ gym-app-front-end/src/Menu.js
@@ -0,0 +1,31 @@
+// Menu.js
+import React from 'react';
+
+const Menu = () => {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="/">JussGym</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/workouts">Workout Plans</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/exercises">Exercises</a>
+              </li>
+              {/* Add more menu items as needed */}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  };
+
+export default Menu;
