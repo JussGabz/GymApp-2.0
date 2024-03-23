4018846a4bbf1d03735fcdf5318c56be3be559b1
JussGabz <gabrieli94@hotmail.co.uk>
2024-03-23 19:04:56 +0000

    add cors middleware + functions

--- gym_app/settings.py
+++ gym_app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'gym_app.admin_app',
 ]
 
@@ -54,7 +55,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'gym_app.urls'
 
