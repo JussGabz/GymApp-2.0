225b78bef83e5a6964eeba7251def335b85e3e11
JussGabz <gabrieli94@hotmail.co.uk>
2024-04-03 18:10:20 +0100

    add video front cloud front aws

--- gym-app-front-end/src/Homepage.js
+++ gym-app-front-end/src/Homepage.js
@@ -13,6 +13,12 @@ const HomePage = () => {
       <h1>Juss Gym</h1>
       <p>Workout with JussGym</p>
       {/* Add more content as needed */}
+      <video width={800} height={500} controls loop autoPlay muted>
+        <source
+          src='https://d164p06em7ia.cloudfront.net/C0201.MP4'
+          type='video/mp4'
+        />
+      </video>
     </div>
   );
 };
