d54e7b948d986520fd8589ca94166b9fae7327ec
JussGabz <gabrieli94@hotmail.co.uk>
2024-04-03 18:09:26 +0100

    remove home menu

--- gym-app-front-end/src/Menu.js
+++ gym-app-front-end/src/Menu.js
@@ -11,9 +11,6 @@ const Menu = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-              </li>
               <li className="nav-item">
                 <a className="nav-link" href="/workouts">Workout Plans</a>
               </li>
