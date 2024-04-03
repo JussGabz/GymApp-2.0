2e96538a5eacd56b93fd5999f448c12cb4728576
JussGabz <gabrieli94@hotmail.co.uk>
2024-04-03 18:11:04 +0100

    breakdown workoutplan to a component

--- gym-app-front-end/src/WorkoutPage.js
+++ gym-app-front-end/src/WorkoutPage.js
@@ -1,52 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import WorkoutPlans from './component/workoutplans.js'
+import './css/styles.css'
 import Menu from './Menu';
 
+
 const WorkoutPage = () => {
-
-    // State to hold the fetched data
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            // Fetch data from your Django endpoint (e.g., list action of your ViewSet)
-            const response = await fetch('http://127.0.0.1:8000/workoutplans/');
-            
-            // Check if the response is successful
-            if (!response.ok) {
-              throw new Error('Failed to fetch data');
-            }
-            
-            // Parse the JSON response
-            const jsonData = await response.json();
-            
-            // Update the component state with the fetched data
-            setData(jsonData);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-    
-        // Call the fetchData function when the component mounts
-        fetchData();
-      }, []); // Empty dependency array ensures the effect runs only once
-    
     return (
-        <div>
-            < Menu />
-            <h1>Workout Plans</h1>
-            <p>Latest Workout Plans</p>
-            {data ? (
-                <ul>
-                    {data.results.map(item => (
-                        <li key={item.id}>{item.name}</li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Loading...</p>
-            )}
-            {/* Add more content as needed */}
-        </div>
+      <div>
+        < Menu />
+        <WorkoutPlans/>
+      </div>
     );
   };
   
---
+++ gym-app-front-end/src/component/workoutplans.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+
+function WorkoutPlan() {
+
+    // State to hold the fetched data
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+            // Fetch data from your Django endpoint (e.g., list action of your ViewSet)
+            const response = await fetch('http://127.0.0.1:8000/workoutplans/');
+            
+            // Check if the response is successful
+            if (!response.ok) {
+                throw new Error('Failed to fetch data');
+            }
+            
+            // Parse the JSON response
+            const jsonData = await response.json();
+            
+            // Update the component state with the fetched data
+            setData(jsonData);
+            } catch (error) {
+            console.error('Error fetching data:', error);
+            }
+        };
+    
+        // Call the fetchData function when the component mounts
+        fetchData();
+        }, []); // Empty dependency array ensures the effect runs only once
+        
+    return (
+        <div className="container">
+            <div style={{ margin: "2rem"}}>
+                <h1 style={{ textAlign: 'center'}}>Work Out Plans</h1>
+            </div>
+            
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                {data ? (
+                    data.results.map((item) => (
+                        <div key={item.id} className="col">
+                            <div className="card h-100">
+                                <img src="images/juss-gym-logo.png" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Workout Plan: {item.name}</h5>
+                                    {item.exercises.map(exercise => (
+                                        <p key={exercise.id} className="card-text">
+                                            {exercise.name}
+                                        </p>
+                                    ))}
+                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <div>No data available</div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default function WorkoutPlans(){
+    return (
+    <WorkoutPlan />
+    )
+}
