35db45741afa65392d4663e908c3c2ca67733477
JussGabz <gabrieli94@hotmail.co.uk>
2024-07-01 16:45:34 +0100

    Change model representation & Avoid exercise dups

--- gym_app/admin_app/models.py
+++ gym_app/admin_app/models.py
@@ -7,9 +7,15 @@ from django.contrib.auth.models import User
 
 class Exercise(models.Model):
     # TODO - Add Date Updated Field for exercise
-    name = models.CharField(max_length=50, null=False)
+    name = models.CharField(max_length=50, null=False, unique=True)
     target_area = models.CharField(max_length=50)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def __repr__(self) -> str:
+        return f"{self.id}: {self.name}"
+    
+    def __str__(self) -> str:
+        return f"{self.id}: {self.name}"
 
 
 class WorkoutPlan(models.Model):
@@ -19,4 +25,7 @@ class WorkoutPlan(models.Model):
     workout_type = models.CharField(max_length=50)
     date_added = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    exercises = models.ManyToManyField(Exercise)
+    exercises = models.ManyToManyField(Exercise, related_name='exercises')
+
+    def __str__(self) -> str:
+        return f"Workout Plan {self.id}: {self.name}"
