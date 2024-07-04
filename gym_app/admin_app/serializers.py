e3d57950e79b5a5a844d1fc9d3bc31f478ed7a54
JussGabz <gabrieli94@hotmail.co.uk>
2024-07-04 10:39:28 +0100

    workout plan serializers create and update

--- gym_app/admin_app/serializers.py
+++ gym_app/admin_app/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import User
 from .models import Exercise, WorkoutPlan
 from rest_framework import serializers
 from gym_app.settings import DATETIME_FORMAT
@@ -17,12 +17,35 @@ class ExerciseSerializer(serializers.ModelSerializer):
 
 class WorkoutPlanSerializer(serializers.ModelSerializer):
 
-    exercises = ExerciseSerializer(many=True, read_only=True)
-    created_by = serializers.PrimaryKeyRelatedField(read_only=True)
+    exercises = ExerciseSerializer(many=True)
+    created_by = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = WorkoutPlan
         fields = ['id', 'name', 'workout_type', 'date_added', 'created_by', 'exercises']
+    
+    # Create Workout Plan with Exercises
+    def create(self, validated_data):
+        # Get Exercise Data
+        print(validated_data)
+        exercises_data = validated_data.pop('exercises')
+
+        workout_plan = WorkoutPlan.objects.create(**validated_data)
+
+        for exercise_data in exercises_data:
+            exercise, created = Exercise.objects.get_or_create(**exercise_data)
+            workout_plan.exercises.add(exercise)
+        return workout_plan
+    
+    def update(self, instance, validated_data):
+        # Get Validated Data from client
+        exercise_data = validated_data.pop('exercises')
+        print(exercise_data)
+
+        # Get Data from instance
+        # Replace Data in instance with client data
+        # instance.exercises = validated_data.get('exercises')
+
 
 
     
