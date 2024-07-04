929778d76108b2ccc74b206ab83c7378fcd6ad1a
JussGabz <gabrieli94@hotmail.co.uk>
2024-07-04 10:44:28 +0100

    enable partial update mixin

--- gym_app/admin_app/views.py
+++ gym_app/admin_app/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import User
 from .models import Exercise, WorkoutPlan
 from rest_framework import permissions, viewsets
 from .permissions import IsUnAuthenticatedReadOnly
- 
+from .utils import EnablePartialUpdateMixin
+
 from gym_app.admin_app.serializers import UserSerializer, ExerciseSerializer, WorkoutPlanSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -15,7 +16,7 @@ class ExerciseViewSet(viewsets.ModelViewSet):
     serializer_class = ExerciseSerializer
     permission_classes = [permissions.IsAuthenticated | IsUnAuthenticatedReadOnly]
 
-class WorkoutPlanViewSet(viewsets.ModelViewSet):
+class WorkoutPlanViewSet(EnablePartialUpdateMixin, viewsets.ModelViewSet):
     queryset = WorkoutPlan.objects.all()
     serializer_class = WorkoutPlanSerializer
     permission_classes = [permissions.IsAuthenticated | IsUnAuthenticatedReadOnly]
