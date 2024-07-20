c47d8c06585f7900011ecad7d7bdd6f4b13936b2
JussGabz <gabrieli94@hotmail.co.uk>
2024-07-20 18:49:36 +0100

    added utils

---
+++ gym_app/admin_app/utils/exercise_utils.py
@@ -0,0 +1,8 @@
+TARGET_AREA_CHOICES = [
+    ("CHEST", "Chest"),
+    ("LEGS", "Legs"),
+    ("BICEPS", "Biceps"),
+    ("TRICEPS", "Triceps"),
+    ("SHOULDERS", "Shoulders"),
+    ("BACK", "Back"),
+]
