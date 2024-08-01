a7c7faab3f437a2110df596bac8cce15700a8f7e
JussGabz <gabrieli94@hotmail.co.uk>
2024-08-01 13:41:44 +0100

    exercise and workout filters

---
+++ gym_app/admin_app/filters.py
@@ -0,0 +1,19 @@
+import django_filters
+from .utils.exercise_utils import TARGET_AREA_CHOICES, WORKOUT_TYPE_CHOICES
+from .models import Exercise, WorkoutPlan
+
+
+class ExerciseFilter(django_filters.FilterSet):
+    target_area = django_filters.ChoiceFilter(choices=TARGET_AREA_CHOICES)
+
+    class Meta:
+        model = Exercise
+        fields = ("target_area",)
+
+
+class WorkOutPlanFilter(django_filters.FilterSet):
+    workout_type = django_filters.ChoiceFilter(choices=WORKOUT_TYPE_CHOICES)
+
+    class Meta:
+        model = WorkoutPlan
+        fields = ("workout_type",)
