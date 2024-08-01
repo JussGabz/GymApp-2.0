9f76759507a411e5d4cb266b3b2d8941565d245f
JussGabz <gabrieli94@hotmail.co.uk>
2024-08-01 13:42:33 +0100

    model refactor and validations

--- gym_app/admin_app/models.py
+++ gym_app/admin_app/models.py
@@ -1,63 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-
-# Create your models here.
+from .validators import validate_exercise_target_area, validate_workout_plan_workouttype
+from .utils.exercise_utils import TARGET_AREA_CHOICES, WORKOUT_TYPE_CHOICES
 
 
 class Exercise(models.Model):
-    CHEST = "CHEST"
-    LEGS = "LEGS"
-    BICEPS = "BICEPS"
-    TRICEPS = "TRICEPS"
-    SHOULDERS = "SHOULDERS"
-    BACK = "BACK"
 
-    TARGET_AREA_CHOICES = [
-        (CHEST, "Chest"),
-        (LEGS, "Legs"),
-        (BICEPS, "Biceps"),
-        (TRICEPS, "Triceps"),
-        (SHOULDERS, "Shoulders"),
-        (BACK, "Back"),
-    ]
-    # TODO - Add Date Updated Field for exercise
     name = models.CharField(max_length=50, null=False, unique=True)
-    target_area = models.CharField(max_length=50, choices=TARGET_AREA_CHOICES)
+    target_area = models.CharField(
+        max_length=50,
+        choices=TARGET_AREA_CHOICES,
+        validators=[validate_exercise_target_area],
+    )
     date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        permissions = [
+            ("readonly_exercises", "can view exercise but not add, change or delete.")
+        ]
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __repr__(self) -> str:
         return f"{self.id}: {self.name}"
-    
+
     def __str__(self) -> str:
         return f"{self.id}: {self.name}"
 
 
 class WorkoutPlan(models.Model):
 
-    CARDIO = "CARDIO"
-    HIIT = "HIIT"
-    LIIT = "LIIT"
-    OTHER = "OTHER"
-    STRENGTH = "STRENGTH"
-    HYPERTROPHY = "HYPERTROPHY"
-
-    TARGET_AREA_CHOICES = [
-        (CARDIO, "Cardio"),
-        (HIIT, "HIIT"),
-        (LIIT, "LIIT"),
-        (OTHER, "Other"),
-        (STRENGTH, "Strength"),
-        (HYPERTROPHY, "Hypertrophy"),
-    ]
-
-
     # TODO - Add Date Updated Field for exercise
     name = models.CharField(max_length=50, null=False)
-    workout_type = models.CharField(max_length=50, choices=TARGET_AREA_CHOICES)
+    workout_type = models.CharField(
+        max_length=50,
+        choices=WORKOUT_TYPE_CHOICES,
+        validators=[validate_workout_plan_workouttype],
+    )
     date_added = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    exercises = models.ManyToManyField(Exercise, related_name='exercises')
+    exercises = models.ManyToManyField(Exercise, related_name="exercises")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return f"Workout Plan {self.id}: {self.name}"
