c22d9b034e4626b9951e625867ba2645fcbcebf2
JussGabz <gabrieli94@hotmail.co.uk>
2024-08-01 13:45:36 +0100

    added filters and workout plan actions

--- gym_app/admin_app/views.py
+++ gym_app/admin_app/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import User
 from .models import Exercise, WorkoutPlan
 from rest_framework import permissions, viewsets
-from .permissions import IsUnAuthenticatedReadOnly
+from .permissions import IsUnAuthenticatedReadOnly, IsAdminOrReadonly
 from .utils.mixin_utils import EnablePartialUpdateMixin
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import ExerciseFilter, WorkOutPlanFilter
+from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 from gym_app.admin_app.serializers import (
     UserSerializer,
@@ -18,16 +24,35 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 class ExerciseViewSet(viewsets.ModelViewSet):
+
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
-    permission_classes = [permissions.IsAuthenticated | IsUnAuthenticatedReadOnly]
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = ExerciseFilter
+    permission_classes = [IsAdminOrReadonly]
 
 
 class WorkoutPlanViewSet(EnablePartialUpdateMixin, viewsets.ModelViewSet):
     queryset = WorkoutPlan.objects.all()
     serializer_class = WorkoutPlanSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = WorkOutPlanFilter
     permission_classes = [permissions.IsAuthenticated | IsUnAuthenticatedReadOnly]
 
     def perform_create(self, serializer):
-        print(serializer)
         serializer.save(created_by=self.request.user)
+    
+    @action(detail=False, methods=['get'], url_path="myworkoutplans")
+    def get_my_workouts(self, request):
+        user = request.user
+        workouts = WorkoutPlan.objects.filter(created_by=user)
+        serializer = self.get_serializer(workouts, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['get'], url_path='myworkoutplans')
+    def get_my_workout_plan_by_id(self, request, pk):
+        workout_plan = WorkoutPlan.objects.get(id=pk)
+        serializer = self.get_serializer(workout_plan)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['post'], url_path='myworkoutplans')
