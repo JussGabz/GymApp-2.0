b530245337053528acf7d8207e796e4ba4f7fe7e
JussGabz <gabrieli94@hotmail.co.uk>
2024-08-05 21:21:54 +0100

    black format

--- gym_app/admin_app/permissions.py
+++ gym_app/admin_app/permissions.py
@@ -12,7 +12,8 @@ class IsUnAuthenticatedReadOnly(permissions.BasePermission):
 
 class CanReadExercisesOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.has_perm('admin_app.view_exercises')
+        return request.user.has_perm("admin_app.view_exercises")
+
 
 class ReadOnlyPermission(permissions.BasePermission):
     """
@@ -22,17 +23,19 @@ class ReadOnlyPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         # CHeck if the request method is in SAFE_METHODS (GET, HEAD, OPTIONS)
         if request.method in permissions.SAFE_METHODS:
-            return request.user.has_perm('admin_app.readonly_exercises')
+            return request.user.has_perm("admin_app.readonly_exercises")
         return False
+
 
 class IsAdminOrReadonly(permissions.BasePermission):
     """
     Custom permission to grant read-only access for non-admins and full access for admins.
     """
+
     def has_permission(self, request, view):
         # Allow read-only (safe) methods for all users
         if request.method in permissions.SAFE_METHODS:
             return request.user and request.user.is_authenticated
-        
+
         # Allow non-safe methods (POST, PUT, DELETE) only for admin users
         return request.user and request.user.is_staff
