06de912325e6b10f2bb1ea9971ca02ecdee0a711
JussGabz <gabrieli94@hotmail.co.uk>
2024-09-18 20:37:14 +0100

    Add log in component

--- gym-app-front-end/src/App.js
+++ gym-app-front-end/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Homepage';
 import WorkoutPage from './WorkoutPage'
 import ExercisePage from './ExercisePage'
+import LogInPage from './LogInPage';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path="/" element={<Homepage />} />
         <Route path="/workouts" Component={WorkoutPage} />
         <Route path="/exercises" Component={ExercisePage} />
+        <Route path="/login" Component={LogInPage} />
       </Routes>
     </Router>
   );
