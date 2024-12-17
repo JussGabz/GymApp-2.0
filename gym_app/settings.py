3a3d020883a751c919f4800ff40ea3274b4f42f9
Gabriel Ifesanya <gabrieli94@hotmail.co.uk>
2024-12-17 00:23:54 +0000

    Update settings.py
    collect static files

--- gym_app/settings.py
+++ gym_app/settings.py
@@ -154,6 +154,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# Directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
