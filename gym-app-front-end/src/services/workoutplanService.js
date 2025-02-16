98eb2c6dbb02e87b48e4944f65d41db91601bd4a
JussGabz <gabrieli94@hotmail.co.uk>
2025-02-16 19:28:38 +0000

    Dynamic Workout Search Service

--- gym-app-front-end/src/services/workoutplanService.js
+++ gym-app-front-end/src/services/workoutplanService.js
@@ -4,17 +4,28 @@ import Error from "../component/Error";
 
 // Fetch Workout Plan from backend workout plan endpoint
 // Isolate the API Call Logic -> Make it easy to replace or modify if api changes
-export const fetchWorkoutPlans = async () => {
+export const fetchWorkoutPlans = async (searchQuery = "") => {
     const token = getToken()
-    const response = await fetch( WORKOUT_PLANS_ENDPOINT, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
+
+    // Append search Query as a query parameter if provided
+
+    const url = searchQuery
+        ? `${WORKOUT_PLANS_ENDPOINT}?name=${encodeURIComponent(searchQuery)}`
+        : WORKOUT_PLANS_ENDPOINT;
+    
+    try {
+        const response = await fetch( url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            }
+        })
+        if (!response.ok) {
+            throw new Error('Failed to fetch workout plans')
         }
-    })
-    if (!response.ok) {
-        throw new Error('Failed to fetch workout plans')
+        return await response.json()
+    } catch (error) {
+        console.error()
     }
-    return await response.json()
 } 
