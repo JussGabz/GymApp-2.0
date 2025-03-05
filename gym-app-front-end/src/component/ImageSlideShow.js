c1eb92fb0dcf9d35417a3489d3e095d98ef175c3
JussGabz <gabrieli94@hotmail.co.uk>
2025-03-05 11:12:41 +0000

    image slider change

--- gym-app-front-end/src/component/ImageSlideShow.js
+++ gym-app-front-end/src/component/ImageSlideShow.js
@@ -48,11 +48,12 @@ const styles = {
     container: {
       position: "relative",
       width: "100%",
-      height: "30vw",
+      height: "650px",
     //   overflow: "hidden",
       margin: "0",
       padding: "0",
-      boxSizing: "border-box"
+      boxSizing: "border-box",
+      zIndex: 1,
     },
     image: {
       width: "100%",
