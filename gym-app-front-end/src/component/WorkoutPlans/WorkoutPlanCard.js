e0e93320e3092c39a60cee9a5088d6cc4f4e29ff
JussGabz <gabrieli94@hotmail.co.uk>
2025-03-05 11:11:10 +0000

    added new workout plan card component

--- gym-app-front-end/src/component/WorkoutPlans/WorkoutPlanCard.js
+++ gym-app-front-end/src/component/WorkoutPlans/WorkoutPlanCard.js
@@ -1,18 +1,18 @@
-import React from "react";
-
-function WorkoutPlanCard({ item }) {
-    return (
-        <div className="col">
-            <div className="card h-100">
-                <img src="images/juss-gym-logo.png" className="card-img-top" alt="WorkoutPlan" />
-                <div className="card-body">
-                    <h5 className="card-title">Workout Plan: {item.name}</h5>
-                    <p className="card-text"><small className="text-muted">Date Added: {item.date_added}</small></p>
-                    <p className="card-text"><small className="text-muted">Created By: {item.created_by}</small></p>
-                </div>
+// Reusable WorkoutCard Component
+const WorkoutCard = ({ workout }) => (
+    <div className='col'>
+        <div className='card h-100'>
+            <img src='images/juss-gym-logo.png' className='card-img-top' alt='...' />
+            <div className='card-body'>
+                <h5 className='card-title'>Workout Plans: {workout.name}</h5>
+                {workout.exercises.map(exercise => (
+                    <p key={exercise.id} className='card-text'>{exercise.name}</p>
+                ))}
+                <p className='card-text'><small className='text-muted'>Date Added: {workout.date_added}</small></p>
+                <p className='card-text'><small className='text-muted'>Created By: {workout.created_by}</small></p>
             </div>
         </div>
-    )
-}
+    </div>
+);
 
-export default WorkoutPlanCard
+export default WorkoutCard
