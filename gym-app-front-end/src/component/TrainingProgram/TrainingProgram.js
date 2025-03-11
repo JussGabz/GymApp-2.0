c606b6e8ffed7ab4377b3322bd8e9d6098a16fc7
JussGabz <gabrieli94@hotmail.co.uk>
2025-03-11 17:26:11 +0000

    training program component

---
+++ gym-app-front-end/src/component/TrainingProgram/TrainingProgram.js
@@ -0,0 +1,20 @@
+import { Button } from "react-bootstrap"
+
+
+const TrainingProgram = () => {
+    return (
+        <div style={{ display: "flex" }}>
+            <div style={{ flex: 1, padding: "10px", borderRight: "1px solid black" }}>
+                <div>Thanks</div>
+            </div>
+            <div style={{ flex: 1, padding: "10px" }}>
+                <p>START TODAY</p>
+                <h2>Training Programs</h2>
+                <div>Download one of the Training Programs - Get In the Best Shape of your Life!</div>
+                <Button>Download</Button>
+            </div>
+        </div>
+
+    )
+}
+export default TrainingProgram
