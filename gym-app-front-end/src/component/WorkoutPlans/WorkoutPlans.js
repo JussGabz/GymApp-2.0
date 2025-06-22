d445526100fcc0aa3f813d3fdcd615b8e0585429
JussGabz <gabrieli94@hotmail.co.uk>
2025-06-22 11:25:07 +0100

    paginate workout plans

--- gym-app-front-end/src/component/WorkoutPlans/WorkoutPlans.js
+++ gym-app-front-end/src/component/WorkoutPlans/WorkoutPlans.js
@@ -3,6 +3,9 @@ import Error from "../Error";
 import WorkoutPlanSearchBar from "./WorkoutPlanSearch";
 import WorkoutCard from "./WorkoutPlanCard";
 import { fetchWorkoutPlans } from "../../services/workoutplanService";
+import { Button } from "react-bootstrap";
+import { WORKOUT_PLANS_ENDPOINT } from "../../config/apiConfig"
+
 
 // Main Workout Plan Component -> Render the Header
 // Fetch the Data using useFetchWorkoutPlans
@@ -10,42 +13,63 @@ import { fetchWorkoutPlans } from "../../services/workoutplanService";
 
 function WorkoutPlans() {
     // const { data, loading, error } = useFetchWorkoutPlans();
+
+    // Set variables for user search query & data from api
     const [ searchQuery, setSearchQuery] = useState('')
-    const [data, setData] = useState(null)
+    const [items, setItems] = useState(null)
+    const [nextUrl, setNextUrl] = useState(null)
+    const [previousUrl, setPreviousUrl] = useState(null)
+    const [currentUrl, setCurrentUrl] = useState(WORKOUT_PLANS_ENDPOINT)
 
     // if (loading) return <SkeletonLoader />
     // if (error) return <Error message={error} />
 
     useEffect(() => {
-        fetchData(searchQuery);
-    }, [searchQuery])
+        fetchData(currentUrl)
+        // .then(res => res.json())
+        // .then(data => {
+        //     setItems(data.results)
+        //     setNextUrl(data.next)
+        //     setPreviousUrl(data.previous)
+        // }
+    // )
+    }, [currentUrl])
 
-    const fetchData = async () => {
+    const fetchData = async (url) => {
         try {
-            const response = await fetchWorkoutPlans(searchQuery)
-            console.log(response)
-            setData(response)
-            if (!response.ok) throw new Error('Failed to fetch data')
-            const jsonData = await response.json()
-            setData(jsonData)
+            const jsonData = await fetchWorkoutPlans(url)
+            console.log(jsonData)
+            setItems(jsonData.results)
+            setNextUrl(jsonData.next)
+            setPreviousUrl(jsonData.previous)
+            
         } catch (error) {
             console.error('Error fetching data:', error)
         } finally {
-
+ 
         }
     }
+
+    const handleSearch = (query) => {
+        setSearchQuery(query)
+        setCurrentUrl(`${WORKOUT_PLANS_ENDPOINT}?name=${encodeURIComponent(query)}`)
+    }
+
     return (
         <div className="container">
             <div style={{ margin: '2rem' }}>
                 <h1 style={{ textAlign: "center" }}>Workout Plans</h1>
             </div>
             <div>
-                <WorkoutPlanSearchBar onSearch={setSearchQuery}/>    
+                <WorkoutPlanSearchBar onSearch={handleSearch}/>    
+            </div>
+            <div>
+                <Button variant="primary" href="/addworkoutplan"> Add New Workout Plan</Button>
             </div>
             {/* {data ? <WorkoutPlanList workoutplans={data.results} /> : <div>No Data Available</div>} */}
-            { data ? (
+            { items ? (
             <div className="row rows-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-                {data.results.map((item) => (
+                {items.map((item) => (
                     <WorkoutCard key={item.id} workout={item} />
                 ))
                 }
@@ -53,7 +77,23 @@ function WorkoutPlans() {
             ) : (
                 <div>No Data Available</div>
             )}
+
+            <div style={{ marginTop: '1rem' }}>
+                <button
+                    onClick={() => setCurrentUrl(previousUrl)}
+                    disabled={!previousUrl}
+                >
+                    Previous
+                </button>
+                <button
+                    onClick={() => setCurrentUrl(nextUrl)}
+                    disabled={!nextUrl}
+                >
+                    Next
+                </button>
+            </div>
         </div>
+        
     )
 }
 
