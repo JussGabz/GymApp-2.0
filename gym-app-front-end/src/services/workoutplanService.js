337ef6b2f7fe2b8564ed1b88028a8853dd2b9a36
JussGabz <gabrieli94@hotmail.co.uk>
2025-06-22 11:23:39 +0100

    default workout plans

--- gym-app-front-end/src/services/workoutplanService.js
+++ gym-app-front-end/src/services/workoutplanService.js
@@ -1,26 +1,23 @@
 import { getToken } from "../utils/tokenUtils";
-import { WORKOUT_PLANS_ENDPOINT } from "../config/apiConfig";
+import { WORKOUT_PLANS_ENDPOINT, API_BASE_URL } from "../config/apiConfig";
 import Error from "../component/Error";
 
 // Fetch Workout Plan from backend workout plan endpoint
 // Isolate the API Call Logic -> Make it easy to replace or modify if api changes
-export const fetchWorkoutPlans = async (searchQuery = "") => {
+export const fetchWorkoutPlans = async (url = WORKOUT_PLANS_ENDPOINT) => {
     const token = getToken()
 
     // Append search Query as a query parameter if provided
-
-    const url = searchQuery
-        ? `${WORKOUT_PLANS_ENDPOINT}?name=${encodeURIComponent(searchQuery)}`
-        : WORKOUT_PLANS_ENDPOINT;
-    
+    console.log(url)
     try {
-        const response = await fetch( url, {
+        const response = await fetch( url , {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 Authorization: `Bearer ${token}`,
             }
         })
+
         if (!response.ok) {
             throw new Error('Failed to fetch workout plans')
         }
