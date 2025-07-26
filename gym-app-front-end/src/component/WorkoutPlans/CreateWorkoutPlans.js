291ea31b206f24385593998d2efa0ec65ac65704
JussGabz <gabrieli94@hotmail.co.uk>
2025-07-26 13:16:00 +0100

    Create WorkoutPage Change

---
+++ gym-app-front-end/src/component/WorkoutPlans/CreateWorkoutPlans.js
@@ -0,0 +1,21 @@
+import React from "react"
+import WorkoutForm from "./WorkoutForm"
+import { Container, Button, Card, Row, Col } from "react-bootstrap"
+import ExerciseDropdown from "./SelectExercises"
+
+function CreateWorkoutPlan() {
+
+    return (
+        <Container className="text-center mt-4">
+            {/* Workout Name */}
+            <p>dsdd</p>
+            <p>dssdvd</p>
+            <ExerciseDropdown/>
+            <WorkoutForm/>
+            
+
+        </Container>
+    )
+}
+
+export default CreateWorkoutPlan
